b1079874ed0065f196510a7c2a1b199634a8af08
Renan Portugal do Rio <renanrpdrpro@gmail.com>
2020-06-01 23:05:14 -0300

    Funções de array: pop, push, shitft etc...

---
+++ EXERCICIOS-JS/07-array/arrayMeotodos.js
@@ -0,0 +1,30 @@
+const pilotos = ['Vettel', 'Aloson', 'Raikkonen', 'Massa']
+
+pilotos.pop() // Remove o ultimo elemento do array
+console.log(pilotos)
+
+pilotos.push('Verstappen')
+console.log(pilotos)
+
+pilotos.shift() // remove o primeiro elemento de um array
+console.log(pilotos)
+
+pilotos.unshift('Hamilton')
+console.log(pilotos)
+
+// Splice pode add e remover elementos
+
+// adicionar
+pilotos.splice(2, 0, 'Bottas', 'Massa')
+console.log(pilotos)
+
+// remover
+pilotos.splice(3, 1) // O massa quebrou de novo
+console.log(pilotos)
+
+// Ele retorna um novo array a partir do elemento passado como parametro
+const algunsPilotos1 = pilotos.slice(2)
+console.log(algunsPilotos1)
+
+const algunsPilotos2 = pilotos.slice(1, 4)
+console.log(algunsPilotos2)
